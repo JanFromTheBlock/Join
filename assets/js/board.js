bfaa578b85d1f2b80c7987b54ceb8c56098b0a26
Jan Borcholt <janborcholt@web.de>
2023-07-20 00:20:41 +0200

    simplify functions

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,9 +1,9 @@
-let taskTitles = ["1", "2", "3", "4"]
-let taskNames = ["To do", "In progress", "Await feedback", "Done"]
-let = currentDraggedElement;
+let taskTitles = ["1", "2", "3", "4"]  //Array, um Drag und Drop Zielarea festzulegen
+let taskNames = ["To do", "In progress", "Await feedback", "Done"]  //Array, um beim Rendern der Rask-areas die Titel zu vergeben
+let = currentDraggedElement;                           // In dieser Variable wird die id der gedraggten Task gespeichert
 
 function addBoardRender() {
-    let board = docID('board');
+    let board = docID('board');  //Suchbereich und Add Task Button werden gerendert
     board.innerHTML = '';
     board.innerHTML += /*html*/`
         <div id="board-input">
@@ -15,11 +15,11 @@ function addBoardRender() {
         </div>
         <div id="task-area"></div>
     `
-    for (let index = 0; index < taskTitles.length; index++) {
-        const taskTitle = taskTitles[index];
-        const taskName = taskNames[index];
-        let indexFinal = index +1
-        docID('task-area').innerHTML += /*html*/`
+    for (let index = 0; index < taskTitles.length; index++) {  //die 4 Task-areas werden gerendert (To dos, in Progress, ...)
+        const taskTitle = taskTitles[index];  //Diese Variable bestimmt, wo das gedarggte Objekt hinverschoben werden soll
+        const taskName = taskNames[index];    //Diese variable weißt den task-areas den dazugehörigen Titel zu
+        let indexFinal = index +1             // index muss um einen erhöht werden, da in diesem Fall bei den tasks bei 1 angefangen wurde zu zählen und nicht wie üblich bei 0
+        docID('task-area').innerHTML += /*html*/` 
             <div ondrop="moveTo('${taskTitle}')" ondragover="allowDrop(event)" class="task-body" id="task-body${index}">
                 <div class="task-body-flex">
                     <span>${taskName}</span>
@@ -37,27 +37,27 @@ function addBoardRender() {
 function loadTasks() {
     emptyTaskDivs();
     let k = 0;         //fortlaufende Variable definiert für die contact-divs
-    for (let id = 0; id < tasks.length; id++) {   //For-Schleife zum Laden der 4 Task-Container
+    for (let id = 0; id < tasks.length; id++) {   //For-Schleife zum Beladen der 4 Task-Container
         renderTaskBody(id);
         renderSubtasks(id);
         renderUrgencySymbol(id);
         renderCategoryColor(id);
 
-        let contactArea = docID('contact-area' + id);
+        let contactArea = docID('contact-area' + id);             //Kontaktbereich in der Task wird gerendert 
         contactArea.innerHTML = ``
-        for (let i = 0; i < tasks[id]['contact-firstname'].length; i++) {
-            let firstName = tasks[id]['contact-firstname'][i];
-            let lastName = tasks[id]['contact-lastname'][i];
-            let Initial1 = firstName.charAt(0);
-            let Initial2 = lastName.charAt(0);
-            let initials = Initial1 + Initial2;
-            let initialsUpper = initials.toLocaleUpperCase();
-            let color = tasks[id]['contact-color'][i];
-            k++;
-            contactArea.innerHTML += /*html*/`
+        for (let i = 0; i < tasks[id]['contact-firstname'].length; i++) {   //for SChleife durch contact-firstname
+            let firstName = tasks[id]['contact-firstname'][i];   //Variable erhält Vornamen an der Position i
+            let lastName = tasks[id]['contact-lastname'][i];     //Variable erhält Nachnamen an der Position i
+            let Initial1 = firstName.charAt(0);                  //Anfangsbuchstabe Vorname
+            let Initial2 = lastName.charAt(0);                   //Anfangsbuchstabe Nachname
+            let initials = Initial1 + Initial2;                  //beide Initialien werden zusammengeführt        
+            let initialsUpper = initials.toLocaleUpperCase();    //Initialien werden als Großbuchstaben geschrieben
+            let color = tasks[id]['contact-color'][i];           //die dazugehörige(i) Farbe wird aus dem Array gezogen
+            k++;                                                 //Variable k erhöht sich um eins und wird an id contacts angehängt
+            contactArea.innerHTML += /*html*/`                  
         <span class="contacts" id = "contacts${k}"> ${initialsUpper}</span>
     `
-            docID('contacts' + k).style.backgroundColor = color;
+            docID('contacts' + k).style.backgroundColor = color;     //Farbe wird für Kontaksymbol geändert
         }
     }
 
@@ -88,41 +88,41 @@ function renderTaskBody(id) {
         </div>
         
     </div>
-`
+`                                    //Taskbody wurde gerendert und dabei wurde den Task-divs Drag-Funktionen zugeteilt, damit man in diesen Bereich draggen kann und Drag-Funktion gestartet werden kann
 }
 
-function startDragging(element){
+function startDragging(element){                          //Funktion um die id der gedraggten Task in der Variable zu speichern; muss auch um 1 gesenkt werden, da bei den ids bei 1 statt bei 0 angefangen wurde
     currentDraggedElement = element - 1;
 
 }
-function allowDrop(ev) {
+function allowDrop(ev) {                                //Funktion die dafür sorgt, dass Tasks gedroppt werden können in dem Bereich
     ev.preventDefault();
   }
 
-  function moveTo(progress){
-    tasks[currentDraggedElement]['progress'] = progress;
-    addBoardRender();
+  function moveTo(progress){                           //Funktion bekommt als Parameter den Ort, wo die gedraggte Task gedropt werden soll
+    tasks[currentDraggedElement]['progress'] = progress;   // anschließend wird dies im array verändert an der entsprechenden Position
+    addBoardRender();                                      // anschließend muss alles neu gerendert werden, damit die Änderungen geladen werden
   }
 
-function renderSubtasks(id) {
-    if (tasks[id]["subtasks"] > 0) {
-        let a = parseInt(tasks[id]["subtasks"])
-        let b = parseInt(tasks[id]["done-tasks"])
-        let percent = b / a * 100
-        docID('progress-bar' + id).classList.remove('d-none')
+function renderSubtasks(id) {                    //subtasks werden gerendert
+    if (tasks[id]["subtasks"] > 0) {             // wenn die Anzahl an Subtasks größer 0 ist dann wird Funktion ausgeführt
+        let a = parseInt(tasks[id]["subtasks"])   // Variable a sind die Anzahl an subtasks
+        let b = parseInt(tasks[id]["done-tasks"]) // Variable b sind die Anzahl erledigter subtasks
+        let percent = b / a * 100                 // Prozentanteil erledigter aufgaben wird berechnet
+        docID('progress-bar' + id).classList.remove('d-none')      //der progress-bar wird das d-none entfernt und sie wird sichtbar
         docID('subtask' + id).innerHTML = /*html*/`
             ${b}/${a} subtasks
-        `;
-        docID('progress-bar-inside' + id).style.width = `${percent}%`;
+        `;                                                          // die Anzahl an subtass wird neben die progress-bar gerendert
+        docID('progress-bar-inside' + id).style.width = `${percent}%`;   //der Prozentanteil erledigter Aufgaben wird als Füllmenge für die progress-bar verwnedet
     }
 }
 
 function renderUrgencySymbol(id) {
     let urgency = tasks[id]["urgency"];
-    docID('contact-area-img' + id).src = "./assets/img/" + urgency + "Logo.png";
+    docID('contact-area-img' + id).src = "./assets/img/" + urgency + "Logo.png";  //je nachdem welche urgency besteht, wird ein anderes Bild gerendert
 }
 
-function renderCategoryColor(id) {
+function renderCategoryColor(id) {                  //die Hintergrundfarbe für die Task-Kategorie wird geladen und dem div gegeben
     let color = tasks[id]["category-color"];
     docID('task-category' + id).style.backgroundColor = color;
 }
