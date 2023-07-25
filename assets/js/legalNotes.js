1e4737d1d39132ab4ed941fe6945dc47ff2a44d5
JanFromTheBlock <janborcholt@web.de>
2023-07-25 10:12:52 +0200

    finish legal Notes

--- assets/js/legalNotes.js
+++ assets/js/legalNotes.js
@@ -6,3 +6,12 @@ function hideElements(){
     document.getElementById('header-user-con').classList.add('d-none');
     document.getElementById('header-help').classList.add('d-none');
 }
+function leavePage(){
+    window.location = './summary.html';
+    document.getElementById('menu-summary').classList.remove('d-none');
+    document.getElementById('menu-board').classList.remove('d-none');
+    document.getElementById('menu-add').classList.remove('d-none');
+    document.getElementById('menu-contacts').classList.remove('d-none');
+    document.getElementById('header-user-con').classList.remove('d-none');
+    document.getElementById('header-help').classList.remove('d-none');
+}
