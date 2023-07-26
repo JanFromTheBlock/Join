55d57989ef0ca2dac1971d182b8f6ce6e2639f88
Andreas Henner <134337273+AndreasHenner@users.noreply.github.com>
2023-07-25 20:34:50 -0500

    addTask update Category

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -1,23 +1,28 @@
 let categories = [
   {
     name: "Design",
-    color: "./assets/img/ellipseOrange.png",
+    img: "./assets/img/ellipseOrange.png",
+    color: "#FF7A00",
   },
   {
     name: "Sales",
-    color: "./assets/img/ellipseRosa.png",
+    img: "./assets/img/ellipseRosa.png",
+    color: "#E200BE",
   },
   {
     name: "Backoffice",
-    color: "./assets/img/ellipseLightblue.png",
+    img: "./assets/img/ellipseLightblue.png",
+    color: "#1FD7C1",
   },
   {
     name: "Media",
-    color: "./assets/img/ellipseBlue.png",
+    img: "./assets/img/ellipseBlue.png",
+    color: "#0038FF",
   },
   {
     name: "Marketing",
-    color: "./assets/img/ellipsegreen.png"
+    img: "./assets/img/ellipsegreen.png",
+    color: "#2AD300"
   }
 ];
 
@@ -86,99 +91,7 @@ let urgency;
 let categoryColor;
 
 
-function addTaskRender(id) {
-  docID("addTask").innerHTML = /*html*/ `
-    <div class="add-task">
-        <form>
-          <input autocomplete="off" id="inputFieldTitle" required class="add-task-title cursor-pointer" placeholder="Enter a title" type="text">
-          <div class="add-task-select-contact-edit">
-             <input autocomplete="off" required onclick ="showContactList()" placeholder="Selected contacts to assign" class="add-task-select-contact cursor-pointer" id="selectContact" type="email"> 
-             <img id="contactSelectArrow" src="./assets/img/selectfieldArrow.png">
-             <div class="d-none" id="editContact">
-               <img onclick="showAddedContact()" class="cursor-pointer" src="./assets/img/logoHaken.png">
-               <img src="./assets/img/seperator.png">
-               <img class="cursor-pointer" onclick="cancelContact()" src="./assets/img/logoCancel.png">
-             </div>
-          </div>
-          <div id="showContacts" class="add-task-hide-contacts add-task-choose-contacts">
-              <span onclick="chooseContact(1)" class="add-task-single-contact">You <img id="chooseBoxContact1" src="./assets/img/logoChooseContact.png"></span>
-              <span onclick="chooseContact(2)" class="add-task-single-contact">Test <img id="chooseBoxContact2" src="./assets/img/logoChooseContact.png"></span>
-              <span onclick="addContact()" class="add-task-single-contact-invite">Invite new contact <img src="./assets/img/logoContactBlue.png"></span>
-          </div>
-          <div class="add-task-initials-area" id="initials"></div>
-        
-        </form>
 
-        <form>
-          <div class="add-task-due-date">
-             <h2>Due Date</h2>
-             <input id="inputDate" class="add-task-due-date-input cursor-pointer cursor-pointer" id="dueDate" type="date">
-          </div>
-        </form>
-
-        <form>
-            <div class="add-task-due-date">
-               <h2>Category</h2>
-               <div class="add-task-select-contact-edit">
-                 <input autocomplete="off" onclick="showCategories()" oninput="markColor()" required class="add-task-select-contact cursor-pointer" id="selectCategory" placeholder="Select Task category">
-                 <img id="categorySelectArrow" src="./assets/img/selectfieldArrow.png">
-                 <div class="add-task-placeholder-color-category">
-                   <img class="d-none" id="placeholderColorCategory" src="${categories[`color`]}">
-                 </div>
-                 <div class="add-task-edit-category d-none" id="editCategory">
-                    <img onclick="finishPushCategoryToArray()" class="cursor-pointer" src="./assets/img/logoHaken.png">
-                    <img src="./assets/img/seperator.png">
-                    <img class="cursor-pointer" onclick="cancelCategory()" src="./assets/img/logoCancel.png">
-                 </div>
-               </div>
-                 <div id="showCategories" class="add-task-hide-contacts add-task-choose-contacts">
-                   <span onclick="newCategory()" class="add-task-single-priority">New category</span>
-                </div>
-                 <div id="categoryColors" class="add-task-category-colors d-none">
-                     <img onclick="addColorToCategory(src)" id="lightBlue" src="./assets/img/ellipseLightblue.png">
-                     <img onclick="addColorToCategory(src)" id="red" src="./assets/img/ellipseRed.png">
-                     <img onclick="addColorToCategory(src)" id="green" src="./assets/img/ellipseGreen.png">
-                     <img onclick="addColorToCategory(src)" id="orange" src="./assets/img/ellipseOrange.png">
-                     <img onclick="addColorToCategory(src)" id="rosa" src="./assets/img/ellipseRosa.png">
-                     <img onclick="addColorToCategory(src)" id="blue" src="./assets/img/ellipseBlue.png">
-                 </div>
-              </div>
-        </form>
-          <div class="add-task-priority">
-            <button id="urgent" onclick="changeColor(id)" class="add-task-button-priority cursor-pointer">Urgent <img id="urgentLogo" src="./assets/img/urgentLogo.png"></button>
-            <button id="medium" onclick="changeColor(id)" class="add-task-button-priority cursor-pointer">Medium <img id="mediumLogo" src="./assets/img/mediumLogo.png"></button>
-            <button id="low" onclick="changeColor(id)" class="add-task-button-priority cursor-pointer">Low <img id="lowLogo" src="./assets/img/lowLogo.png"></button>
-          </div>
-       <form class="add-task-description-form">
-        <div class="add-task-description">
-          <h2>Description</h2>
-          <textarea required id="description" placeholder="Enter a Description" class="add-task-textarea cursor-pointer"></textarea>
-        </div>
-        </form>
-        
-        <div class="subtask">
-
-        <div id="test"></div>
-          <div>
-              <h2>Subtasks</h2>
-             <div>
-                <input id="inputSubtask" required class="add-task-subtask cursor-pointer" placeholder="Add new subtask" type="text">
-                <img onclick="showSubtasks()" class="add-task-plus-button cursor-pointer" src="./assets/img/subtaskPlus.png">
-              </div>
-
-                <div id="subTaskArea" class="d-none"></div>
-             </div>
-
-           <div class="add-task-button">
-            <button onclick="clearTask()" class="add-task-button-clear cursor-pointer">Clear<img src="./assets/img/xClear.png"></button>  
-            <button onclick="newTask(urgency)" class="add-task-button-create cursor-pointer">Add Task<img src="./assets/img/hakenCreateTask.png"></button>
-          </div>
-
-        </div>
-    </div>
-    <img id="taskAddedToBoard" class="task-added-to-board-hide" src="./assets/img/logoAddedToBoard.png">
-    `;
-}
 
 function showSubtasks() {
   let subtaskArea = document.getElementById(`subTaskArea`);
@@ -410,13 +323,12 @@ function finishPushCategoryToArray(){
 
 function showAddedCategory() {
   let showCategories = document.getElementById(`showCategories`);
-  let colorOfCategory = placeholderColorCategory.src;
-
+ 
   for (let i = 0; i < categories.length; i++) {
     const category = categories[i];
     
     showCategories.innerHTML += /*html*/ `<span id="savedCategory${i}" onclick="chooseCategory(${i})" class="add-task-single-priority">
-    ${category[`name`]}<img src=${category['color']}></span>`;
+    ${category[`name`]}<img src=${category['img']}></span>`;
   }
 }
 
@@ -446,18 +358,17 @@ function addColorToCategory(id) {
   placeholderColorCategory.src = `${colorOfCategory}`;
   placeholderColorCategory.classList.remove(`d-none`);
 
-
-  if(id == "http://127.0.0.1:5500/assets/img/ellipseGreen.png"){
+  if(id == "http://127.0.0.1:5500/assets/img/ellipseGreen.png" || "./assets/img/ellipseGreen.png"){
     categoryColor = "#2AD300";
-  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseOrange.png"){
+  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseOrange.png" || "./assets/img/ellipseOrange.png"){
     categoryColor = "#FF7A00";
-  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseLightblue.png"){
+  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseLightblue.png" || "./assets/img/ellipseLightblue.png"){
     categoryColor = "#1FD7C1";
-  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseRed.png"){
+  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseRed.png" || "./assets/img/ellipseRed.png"){
     categoryColor = "#FF0000";
-  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseBlue.png"){
+  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseBlue.png" || "./assets/img/ellipseBlue.png"){
     categoryColor = "#0038FF";
-  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseRosa.png"){
+  }else if(id == "http://127.0.0.1:5500/assets/img/ellipseRosa.png" || "./assets/img/ellipseRosa.png"){
     categoryColor = "#E200BE";
   }
   pushCategoryToArray(categoryColor);
@@ -468,7 +379,8 @@ function chooseCategory(i) {
   let savedCategory = document.getElementById(`savedCategory${i}`);
   let showCategories = document.getElementById(`showCategories`);
   let selectCategory = document.getElementById(`selectCategory`);
-  
+  let categoryImg = categories[i][`img`];
+ 
   // Zugriff auf das <img>-Element innerhalb des savedCategory-Containers
   let savedCategoryImg = savedCategory.querySelector("img");
   // Ersetze das src-Attribut des placeholderColorCategory-Bildes mit dem des savedCategory-Bildes
@@ -480,6 +392,7 @@ function chooseCategory(i) {
   placeholderColorCategory.classList.remove(`d-none`);
   selectCategory.style.paddingLeft = "0";
   showCategories.classList.add(`d-none`);
+  addColorToCategory(categoryImg);
 }
 
 function markColor(){
@@ -489,7 +402,7 @@ function markColor(){
     document.getElementById("rosa").style.boxShadow = "0 4px 4px 0 rgba(0, 0, 0, 0.25)";
     categories.push("#2AD300");
   }else if(selectCategory.includes("Backoffice")){
-    document.getElementById("lightBlue").style.boxShadow = "0 4px 4px 0 rgba(0, 0, 0, 0.25)";
+    document.getElementById("lightBlue3,").style.boxShadow = "0 4px 4px 0 rgba(0, 0, 0, 0.25)";
   }else if(selectCategory.includes("Design")){
     document.getElementById("orange").style.boxShadow = "0 4px 4px 0 rgba(0, 0, 0, 0.25)";
   }else if(selectCategory.includes("Marketing")){
