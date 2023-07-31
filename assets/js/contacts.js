33190b7665f346b2fa02901c566aa7d9e3091651
JanFromTheBlock <janborcholt@web.de>
2023-07-31 19:29:46 +0200

    renderContentDisplay Funktion

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -29,7 +29,6 @@ let contacts = {
             color: "orange"
         }
     ]
-
 }
 
 function renderContacts() {
@@ -56,8 +55,9 @@ function renderContacts() {
             initials = initials.replace(/\s/g, '');
             let mail = element[id].mail;
             let color = element[id].color;
+            let display = element[id];
             contact.innerHTML += /*html*/`
-                <div class="contact" id="contact${id}">
+                <div class="contact" id="contact${id}" onclick="renderContactDisplay('${encodeURIComponent(JSON.stringify(display))}')">
                     <div class="contact-sign" id="contact-sign${NumberofContacts}">${initials}</div>
                     <div id="contact-data">
                         <div id="name">${name}</div>
@@ -68,4 +68,43 @@ function renderContacts() {
             docID('contact-sign' + NumberofContacts).style.backgroundColor = color;
         }
     }
+}
+
+function renderContactDisplay(elementJSON) {
+    const element = JSON.parse(decodeURIComponent(elementJSON));
+
+    let contactDisplay = docID('contact-display');
+    let name = element.name;
+    let initials = name.replace(/[a-z]/g, '');
+    initials = initials.replace(/\s/g, '');
+    let mail = element.mail;
+    let color = element.color;
+    let phone = element.phone;
+    contactDisplay.classList.remove('d-none');
+    contactDisplay.innerHTML = '';
+    contactDisplay.innerHTML += /*html*/`
+        <div id="contact-header">
+            <div id="contact-icon">${initials}</div>
+            <div id="contact-actions">
+                <div id="contact-display-name">${name}</div>
+                <div id="contact-imgs">
+                    <div class="contact-img"><img src="./assets/img/edit_contact.png">Edit</div>
+                    <div class="contact-img"><img src="./assets/img/delete_contact.png">Delete</div>
+                </div>
+            </div>
+        </div>
+        <div id="contact-body">
+            <div id="contact-information">Contact Information</div>
+            <div id="contact-mail-phone">
+                <div id="contact-mail">
+                    <span id="contact-mail-title">E-Mail</span>
+                    <span id="contact-mail-adress">${mail}</span>
+                </div>
+                <div id="contact-phone">
+                    <span id="contact-phone-title">Phone</span>
+                    <span id="contact-phone-number">${phone}</span>
+                </div>
+            </div>
+        </div>
+    `;
 }
